1f036c4feae1790917dcfcde4b4175e0487a9704
parmarkriss <krissparmar2003@gmail.com>
2024-11-22 04:23:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+  
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden'); // Toggles the visibility of the menu
+});
